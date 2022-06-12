11281cc9f894aa55625c3785361513f5ce7bed9a
Muhammad Azib Ali <muhammadazibali20@gmail.com>
2022-06-13 03:36:44 +0500

    Initial Push 13-June-2022

---
+++ QRSCS/Models/PsychologicalAssessmentModelDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using QRSCS.Common.PA;
+using System.ComponentModel.DataAnnotations;
+
+namespace QRSCS.Models
+{
+    public class PsychologicalAssessmentModelDTO
+    {
+        public PsychologicalAssessmentModel psychologicalAssessment { get; set; }
+        public TestsPAModel testsPA { get; set; }
+
+    }
+}
